7769b48f9c597634755333cb19fdb122ab7ba91e
Yasinthon <yasinthon_r@cmu.ac.th>
2022-08-04 17:30:24 +0700

    1.6

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
+  <div id="flashMessage" v-if="GStore.flashMessage">
+    {{ GStore.flashMessage }}
+  </div>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'EventList' }">Home</router-link> |
+    <router-link :to="{ name: 'about' }">About</router-link>
   </nav>
   <router-view />
 </template>
-
+<script>
+export default {
+  inject: ['GStore']
+}
+</script>
 <style>
+@keyframes yellowfade {
+  from {
+    background: yellow;
+  }
+  to {
+    background: transparent;
+  }
+}
+#flashMessage {
+  animation-name: yellowfade;
+  animation-duration: 3s;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,6 +46,7 @@ nav a {
 nav a.router-link-exact-active {
   color: #42b983;
 }
+
 h4 {
   font-size: 20px;
 }
--- src/views/event/EventEditView.vue
+++ src/views/event/EventEditView.vue
@@ -1,9 +1,24 @@
 <template>
-  <p>Edit the event here</p>
+  <p>Edit form here</p>
+  <button @click="edit">Edit here</button>
 </template>
 
 <script>
 export default {
-  props: ['id', 'event']
+  props: ['id', 'event'],
+  inject: ['GStore'],
+  methods: {
+    edit() {
+      this.GStore.flashMessage =
+        'You are successfully edit for ' + this.event.title
+      setTimeout(() => {
+        this.GStore.flashMessage = ''
+      }, 3000)
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
